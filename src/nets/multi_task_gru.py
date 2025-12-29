785b201455fd581114e1efa63c4e64a97a1b67d6
DaoChaShao <27882272@qq.com>
2025-12-29 21:48:24 +0800

    refactor: rename BaseRNN to BaseANN in multi_task_gru.py

--- src/nets/multi_task_gru.py
+++ src/nets/multi_task_gru.py
@@ -12,10 +12,10 @@ from torch import (Tensor, nn,
 from typing import override, Literal
 
 from src.configs.cfg_types import Tasks
-from src.nets.base_rnn import BaseRNN
+from src.nets.base_ann import BaseANN
 
 
-class MultiTaskGRU(BaseRNN):
+class MultiTaskGRU(BaseANN):
     def __init__(self,
                  vocab_size: int, embedding_dim: int, hidden_size: int, num_layers: int,
                  dropout_rate: float = 0.3, bidirectional: bool = True,

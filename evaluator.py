45760293772f2cb2673d524d415814d64387a6ae
DaoChaShao <27882272@qq.com>
2026-01-01 15:47:14 +0800

    feat: refactor evaluator to use Seq2SeqTransformerNet and update configuration paths

--- evaluator.py
+++ evaluator.py
@@ -12,9 +12,9 @@ from pathlib import Path
 from torch import Tensor, tensor, device, no_grad, long
 from tqdm import tqdm
 
-from src.configs.cfg_rnn import CONFIG4RNN
-from src.configs.cfg_types import Languages, Tokens, SeqMergeMethods, AttnScorer
-from src.nets.seq2seq_attn_gru import SeqToSeqGRUWithAttn
+from src.configs.cfg_seq2seq_transformer import CONFIG4S2STF
+from src.configs.cfg_types import Languages, Tokens
+from src.nets.seq2seq_transformer import Seq2SeqTransformerNet
 from src.utils.helper import Timer
 from src.utils.highlighter import starts, lines, green
 from src.utils.NLTK import bleu_score
@@ -28,7 +28,7 @@ def connect_db() -> list[tuple]:
     """ Connect to the Database and Return the Connection Object """
     table: str = "translater"
     cols: dict = {"en": str, "cn": str}
-    path: Path = Path(CONFIG4RNN.FILEPATHS.SQLITE)
+    path: Path = Path(CONFIG4S2STF.FILEPATHS.SQLITE)
 
     with SQLiteIII(table, cols, path) as db:
         data = db.fetch_all(col_names=[col for col in cols.keys()])
@@ -68,9 +68,9 @@ def main() -> None:
         # print(en_items[:3])
 
         # Load dictionary
-        dic_cn: Path = Path(CONFIG4RNN.FILEPATHS.DICTIONARY_CN)
+        dic_cn: Path = Path(CONFIG4S2STF.FILEPATHS.DICTIONARY_CN)
         dictionary_cn: dict = load_json(dic_cn) if dic_cn.exists() else print("Dictionary file not found.")
-        dic_en: Path = Path(CONFIG4RNN.FILEPATHS.DICTIONARY_EN)
+        dic_en: Path = Path(CONFIG4S2STF.FILEPATHS.DICTIONARY_EN)
         dictionary_en: dict = load_json(dic_en) if dic_en.exists() else print("Dictionary file not found.")
         reversed_dict: dict = {idx: word for word, idx in dictionary_en.items()}
         # print(reversed_dict)
@@ -90,147 +90,99 @@ def main() -> None:
         ****************************************************************
         """
         # Load the save model parameters
-        parameters: dict[str, list[str]] = {
-            # "gru": [
-            # CONFIG4RNN.FILEPATHS.NET_FALSE_GRU_BEAM_CONCAT,
-            # CONFIG4RNN.FILEPATHS.NET_FALSE_GRU_BEAM_MEAN,
-            # CONFIG4RNN.FILEPATHS.NET_FALSE_GRU_GREEDY_CONCAT,
-            # CONFIG4RNN.FILEPATHS.NET_FALSE_GRU_GREEDY_MEAN,
-            #     CONFIG4RNN.FILEPATHS.NET_TRUE_GRU_BEAM_CONCAT,
-            #     CONFIG4RNN.FILEPATHS.NET_TRUE_GRU_BEAM_MEAN,
-            #     CONFIG4RNN.FILEPATHS.NET_TRUE_GRU_GREEDY_CONCAT,
-            #     CONFIG4RNN.FILEPATHS.NET_TRUE_GRU_GREEDY_MEAN,
-            # ],
-            # "lstm": [
-            #     CONFIG4RNN.FILEPATHS.NET_FALSE_LSTM_BEAM_CONCAT,
-            #     CONFIG4RNN.FILEPATHS.NET_TRUE_LSTM_BEAM_CONCAT,
-            # ],
-            # "rnn": [
-            #     CONFIG4RNN.FILEPATHS.NET_FALSE_RNN_BEAM_CONCAT,
-            #     CONFIG4RNN.FILEPATHS.NET_TRUE_RNN_BEAM_CONCAT,
-            # ],
-            "gru_without_attn": [
-                CONFIG4RNN.FILEPATHS.NET_TRUE_GRU_WITHOUT_ATTN_BEAM_CONCAT,
-            ],
-            "lstm_without_attn": [
-                CONFIG4RNN.FILEPATHS.NET_TRUE_LSTM_WITHOUT_ATTN_BEAM_CONCAT,
-            ],
-            "rnn_without_attn": [
-                CONFIG4RNN.FILEPATHS.NET_TRUE_RNN_WITHOUT_ATTN_BEAM_CONCAT,
-            ],
-            "gru_with_attn": [
-                # CONFIG4RNN.FILEPATHS.NET_TRUE_GRU_WITH_ATTN_BAHDANAU_BEAM_CONCAT,
-                CONFIG4RNN.FILEPATHS.NET_TRUE_GRU_WITH_ATTN_DOT_BEAM_CONCAT,
-                # CONFIG4RNN.FILEPATHS.NET_TRUE_GRU_WITH_ATTN_SDOT_BEAM_CONCAT,
-            ],
-            "lstm_with_attn": [
-                CONFIG4RNN.FILEPATHS.NET_TRUE_LSTM_WITH_ATTN_DOT_BEAM_CONCAT,
-            ],
-            "rnn_with_attn": [
-                CONFIG4RNN.FILEPATHS.NET_TRUE_RNN_WITH_ATTN_DOT_BEAM_CONCAT,
-            ],
-        }
-        for name, paths in parameters.items():
-            if name == "gru_with_attn":
-                print(f"Evaluating GRU Models:")
-                for parameter in paths:
-                    params: Path = Path(parameter)
-                    if params.exists():
-                        print(f"Model {green(params.name)} Exists!")
+        params: Path = Path(CONFIG4S2STF.FILEPATHS.NET_GREEDY_100)  # Net Greedy 100 Model
+        # params: Path = Path(CONFIG4S2STF.FILEPATHS.NET_BEAM_5_100)  # Net Beam 5 100 Model
+        if params.exists():
+            print(f"Model {green(params.name)} Exists!")
 
-                        # model with attention
-                        model = SeqToSeqGRUWithAttn(
-                            vocab_size_src=len(dictionary_cn),
-                            vocab_size_tgt=len(dictionary_en),
-                            embedding_dim=CONFIG4RNN.PARAMETERS.EMBEDDING_DIM,
-                            hidden_size=CONFIG4RNN.PARAMETERS.HIDDEN_SIZE,
-                            num_layers=CONFIG4RNN.PARAMETERS.LAYERS,
-                            dropout_rate=CONFIG4RNN.PREPROCESSOR.DROPOUT_RATIO,
-                            bidirectional=True,
-                            accelerator=CONFIG4RNN.HYPERPARAMETERS.ACCELERATOR,
-                            PAD_SRC=dictionary_cn[Tokens.PAD],
-                            PAD_TGT=dictionary_en[Tokens.PAD],
-                            SOS=dictionary_cn[Tokens.SOS],
-                            EOS=dictionary_cn[Tokens.EOS],
-                            merge_method=SeqMergeMethods.CONCAT,
-                            teacher_forcing_ratio=0.5,
-                            use_attention=True,
-                            attn_scorer=AttnScorer.DOT_PRODUCT
-                        )
-                        model.load_model(params, strict=True)
-                        model.eval()
-                        print("Model Loaded Successfully!")
+            model = Seq2SeqTransformerNet(
+                vocab_size_src=len(dictionary_cn),
+                vocab_size_tgt=len(dictionary_en),
+                embedding_dims=CONFIG4S2STF.PARAMETERS.EMBEDDING_DIMS,
+                scaler=CONFIG4S2STF.PARAMETERS.SCALER,
+                max_len=CONFIG4S2STF.PARAMETERS.MAX_LEN,
+                num_heads=CONFIG4S2STF.PARAMETERS.HEADS,
+                feedforward_dims=CONFIG4S2STF.PARAMETERS.FEEDFORWARD_DIMS,
+                num_layers=CONFIG4S2STF.PARAMETERS.LAYERS,
+                dropout=CONFIG4S2STF.PREPROCESSOR.DROPOUT_RATIO,
+                activation="relu",
+                accelerator=CONFIG4S2STF.HYPERPARAMETERS.ACCELERATOR,
+                PAD=dictionary_cn[Tokens.PAD],
+                SOS=dictionary_cn[Tokens.SOS],
+                EOS=dictionary_cn[Tokens.EOS],
+            )
+            model.load_params(params, strict=True)
+            model.eval()
+            print("Model Loaded Successfully!")
 
-                        # Convert sentences to sequence using dictionary
-                        sequences: list[list[int]] = build_word2id_seqs(cn_items, dictionary_cn, UNK=Tokens.UNK)
-                        # print(sequences[:3])
+            # Convert sentences to sequence using dictionary
+            sequences: list[list[int]] = build_word2id_seqs(cn_items, dictionary_cn, UNK=Tokens.UNK)
+            # print(sequences[:3])
 
-                        # Predict and Evaluate
-                        with no_grad():
-                            strategy: str = params.name.split("-")[6]
-
-                            assert len(sequences) == len(en4prove), "src and truth tgt length mismatch."
-                            results: list[tuple[list[str], list[str], float]] = []
-                            for seq, reference in tqdm(
-                                    zip(sequences, en_items), total=len(sequences), desc=f"{strategy} Model:"
-                            ):
-                                src: Tensor = tensor(seq, dtype=long,
-                                                     device=device(CONFIG4RNN.HYPERPARAMETERS.ACCELERATOR))
-                                out = model.generate(src.unsqueeze(dim=0))
-                                pred = [reversed_dict.get(idx, Tokens.UNK) for idx in out.squeeze().tolist()]
-                                hypothesis: list[str] = [word.strip() for word in pred if word != Tokens.EOS]
-                                # print(hypothesis)
-                                # print(reference)
-                                bleu = bleu_score(reference, hypothesis)
-                                # print(f"BLEU Score: {bleu:.4f}")
-                                results.append((reference, hypothesis, bleu))
-                            starts()
-                            print(f"Evaluation Results for {strategy} Model:")
-                            scores: list[float] = [score for _, _, score in results]
-                            lines()
-                            print(f"Sentence Amount: {len(scores)}")
-                            print(f"Mean BLEU:       {np_mean(scores):.4f}")
-                            print(f"Highest BLEU:    {np_max(scores):.4f}")
-                            print(f"Lowest BLEU:     {np_min(scores):.4f}")
-                            print(f"Median BLEU:     {np_median(scores):.4f}")
-                            starts()
-                            """
-                            ****************************************************************
-                            Evaluation Results for beam Model - bahdanau:
-                            ----------------------------------------------------------------
-                            Sentence Amount: 4374
-                            Mean BLEU:       0.2159 (-0.0209)
-                            Highest BLEU:    1.0000
-                            Lowest BLEU:     0.0000
-                            Median BLEU:     0.1156
-                            ----------------------------------------------------------------
-                            "bid": true, "epoch": 51/100, "strategy": "beam", "merge": "concat", "bleu": 0.2368, "rouge": 0.6262, "attn": true, "score": "bahdanau"
-                            ****************************************************************
-                            ****************************************************************
-                            Evaluation Results for beam Model - dot:
-                            ----------------------------------------------------------------
-                            Sentence Amount: 4374
-                            Mean BLEU:       0.1513 (-0.0327)
-                            Highest BLEU:    1.0000
-                            Lowest BLEU:     0.0000
-                            Median BLEU:     0.0847
-                            ----------------------------------------------------------------
-                            "bid": true, "epoch": 57/100, "strategy": "beam", "merge": "concat", "bleu": 0.1840, "rouge": 0.5721, "attn": true, "score": "dot"
-                            ****************************************************************
-                            ****************************************************************
-                            Evaluation Results for beam Model - sdot:
-                            ----------------------------------------------------------------
-                            Sentence Amount: 4374
-                            Mean BLEU:       0.1976 (-0.0315)
-                            Highest BLEU:    1.0000
-                            Lowest BLEU:     0.0000
-                            Median BLEU:     0.1065
-                            ----------------------------------------------------------------
-                            "bid": true, "epoch": 49/100, "strategy": "beam", "merge": "concat", "bleu": 0.2291, "rouge": 0.6174, "attn": true, "score": "scaled_dot"
-                            ****************************************************************
-                            """
-                    else:
-                        print(f"Model {params.name} does not exist!")
+            # Predict and Evaluate
+            with no_grad():
+                assert len(sequences) == len(en4prove), "src and truth tgt length mismatch."
+                results: list[tuple[list[str], list[str], float]] = []
+                for seq, reference in tqdm(
+                        zip(sequences, en_items), total=len(sequences), desc=f"Model Evaluation:"
+                ):
+                    src: Tensor = tensor(seq, dtype=long, device=device(CONFIG4S2STF.HYPERPARAMETERS.ACCELERATOR))
+                    memory: Tensor = model.encoder(src.unsqueeze(dim=0))
+                    memory_key_padding_mask: Tensor = (src.unsqueeze(dim=0) == dictionary_cn[Tokens.PAD])
+                    out = model.generate(
+                        memory,
+                        memory_key_padding_mask,
+                        top_k=CONFIG4S2STF.PARAMETERS.TOP_K,
+                        top_p=CONFIG4S2STF.PARAMETERS.TOP_P,
+                        temperature=CONFIG4S2STF.PREPROCESSOR.TEMPERATURE,
+                        beams=CONFIG4S2STF.PARAMETERS.BEAMS,
+                        early_stopper=CONFIG4S2STF.PARAMETERS.STOPPER,
+                        do_sample=CONFIG4S2STF.PARAMETERS.SAMPLER,
+                        length_penalty=CONFIG4S2STF.PARAMETERS.LEN_PENALTY_FACTOR
+                    )
+                    pred = [reversed_dict.get(idx, Tokens.UNK) for idx in out.squeeze().tolist()[1:]]
+                    hypothesis: list[str] = [word.strip() for word in pred if word != Tokens.EOS]
+                    # print(hypothesis)
+                    # print(reference)
+                    bleu = bleu_score(reference, hypothesis)
+                    # print(f"BLEU Score: {bleu:.4f}")
+                    results.append((reference, hypothesis, bleu))
+                starts()
+                print(f"Evaluation Results for greedy Model:")
+                scores: list[float] = [score for _, _, score in results]
+                lines()
+                print(f"Sentence Amount: {len(scores)}")
+                print(f"Mean BLEU:       {np_mean(scores):.4f}")
+                print(f"Highest BLEU:    {np_max(scores):.4f}")
+                print(f"Lowest BLEU:     {np_min(scores):.4f}")
+                print(f"Median BLEU:     {np_median(scores):.4f}")
+                starts()
+                """
+                ****************************************************************
+                Evaluation Results for greedy Model:
+                ----------------------------------------------------------------
+                Sentence Amount: 4374
+                Mean BLEU:       0.2555 (-0.0133 from last eval)
+                Highest BLEU:    1.0000
+                Lowest BLEU:     0.0000
+                Median BLEU:     0.1282
+                ----------------------------------------------------------------
+                "epoch": 92/200, "strategy": "greedy", "bleu": 0.2688, "rouge": 0.6271, "avg_pred_len": 7.5065, "perplexity": 7.3681
+                ****************************************************************
+                ****************************************************************
+                Evaluation Results for beam5 Model:
+                ----------------------------------------------------------------
+                Sentence Amount: 4374
+                Mean BLEU:       0.2617 (+0.0501 from last eval)
+                Highest BLEU:    1.0000
+                Lowest BLEU:     0.0000
+                Median BLEU:     0.1341
+                ----------------------------------------------------------------
+                "epoch": 95/200, "strategy": "beam",   "bleu": 0.2116, "rouge": 0.5478, "avg_pred_len": 4.9998, "perplexity": 7.4765
+                ****************************************************************
+                """
+        else:
+            print(f"Model {params.name} does not exist!")
 
 
 if __name__ == "__main__":

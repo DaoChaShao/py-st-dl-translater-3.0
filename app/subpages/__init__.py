b6cc4476a0933d4eafa224d9292da2b02dba74bf
DaoChaShao <27882272@qq.com>
2026-01-01 15:46:46 +0800

    feat: add __init__.py for multi-page structure in Next Word Prediction app

---
+++ app/subpages/__init__.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3.12
+# -*- Coding: UTF-8 -*-
+# @Time     :   2025/12/5 20:17
+# @Author   :   Shawn
+# @Version  :   Version 0.1.0
+# @File     :   __init__.py
+# @Desc     :   
+
+"""
+****************************************************************
+Subpages for Next Word Prediction Streamlit Application
+----------------------------------------------------------------
+Automatically loaded by Streamlit as multi-page structure.
+****************************************************************
+"""
+
+__author__ = "Shawn Yu"
+__version__ = "0.2.0"
+
+__all__ = [
+    "home",
+    "prediction"
+]
